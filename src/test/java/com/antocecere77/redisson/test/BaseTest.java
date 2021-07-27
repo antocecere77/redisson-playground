328ea853485c451cc8b97b69887380247ee0f81d
antocecere77 <antocecere77@gmail.com>
2021-07-27 07:15:56 +0200

    base test

---
+++ src/test/java/com/antocecere77/redisson/test/BaseTest.java
@@ -0,0 +1,24 @@
+package com.antocecere77.redisson.test;
+
+import com.antocecere77.redisson.test.config.RedissonConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.redisson.api.RedissonReactiveClient;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class BaseTest {
+
+    private RedissonConfig redissonConfig;
+    protected RedissonReactiveClient client;
+
+    @BeforeAll
+    public void setClient() {
+        this.client = this.redissonConfig.getReactiveClient();
+    }
+
+    @AfterAll
+    public void shutDown() {
+        this.client.shutdown();
+    }
+}
