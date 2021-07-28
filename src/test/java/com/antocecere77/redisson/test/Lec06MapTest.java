8d826052c76455c98a69973247bfa9ac586ac804
antocecere77 <antocecere77@gmail.com>
2021-07-28 07:50:15 +0200

    map - part 2

--- src/test/java/com/antocecere77/redisson/test/Lec06MapTest.java
+++ src/test/java/com/antocecere77/redisson/test/Lec06MapTest.java
@@ -1,11 +1,14 @@
 package com.antocecere77.redisson.test;
 
+import com.antocecere77.redisson.test.dto.Student;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RMapReactive;
 import org.redisson.client.codec.StringCodec;
+import org.redisson.codec.TypedJsonJacksonCodec;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.util.List;
 import java.util.Map;
 
 public class Lec06MapTest extends BaseTest {
@@ -31,6 +34,34 @@ public class Lec06MapTest extends BaseTest {
                 "city", "miami");
 
         StepVerifier.create(map.putAll(javaMap).then())
+                .verifyComplete();
+    }
+
+    @Test
+    public void mapTest3() {
+
+        //Map<Integer,Student>
+        TypedJsonJacksonCodec codec = new TypedJsonJacksonCodec(Integer.class, Student.class);
+        RMapReactive<Integer, Student> map = this.client.getMap("users", codec);
+
+        Student student1 =  Student.builder()
+                .name("sam")
+                .age(10)
+                .city("atlanta")
+                .marks(List.of(1, 2, 3))
+                .build();
+
+        Student student2 =  Student.builder()
+                .name("jake")
+                .age(30)
+                .city("miami")
+                .marks(List.of(10, 20, 30))
+                .build();
+
+        Mono<Student> mono1 = map.put(1, student1);
+        Mono<Student> mono2 = map.put(2, student2);
+
+        StepVerifier.create(mono1.concatWith(mono2))
                 .verifyComplete();
     }
 }
