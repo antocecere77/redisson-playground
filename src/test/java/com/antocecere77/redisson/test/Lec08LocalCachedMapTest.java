0dcc3418e5ce465f572a96f1a14a598bd2be622b
antocecere77 <antocecere77@gmail.com>
2021-07-29 07:08:01 +0200

    local cached map - part 2

--- src/test/java/com/antocecere77/redisson/test/Lec08LocalCachedMapTest.java
+++ src/test/java/com/antocecere77/redisson/test/Lec08LocalCachedMapTest.java
@@ -8,6 +8,10 @@ import org.redisson.api.LocalCachedMapOptions;
 import org.redisson.api.RLocalCachedMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.codec.TypedJsonJacksonCodec;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.List;
 
 public class Lec08LocalCachedMapTest extends BaseTest {
 
@@ -19,8 +23,8 @@ public class Lec08LocalCachedMapTest extends BaseTest {
         RedissonClient redissonClient = config.getClient();
 
         LocalCachedMapOptions<Integer, Student> mapOptions = LocalCachedMapOptions.<Integer, Student>defaults()
-                .syncStrategy(LocalCachedMapOptions.SyncStrategy.NONE)
-                .reconnectionStrategy(LocalCachedMapOptions.ReconnectionStrategy.CLEAR);
+                .syncStrategy(LocalCachedMapOptions.SyncStrategy.UPDATE)
+                .reconnectionStrategy(LocalCachedMapOptions.ReconnectionStrategy.NONE);
 
         this.studentsMap = redissonClient.getLocalCachedMap(
                 "students",
@@ -29,6 +33,27 @@ public class Lec08LocalCachedMapTest extends BaseTest {
         );
     }
 
+    @Test
+    public void appServer1() {
+        Student student1 =  Student.builder().name("sam").age(10).city("atlanta").marks(List.of(1, 2, 3)).build();
+        Student student2 =  Student.builder().name("jake").age(30).city("miami").marks(List.of(10, 20, 30)).build();
+
+        this.studentsMap.put(1, student1);
+        this.studentsMap.put(2, student2);
+
+        Flux.interval(Duration.ofSeconds(1))
+                .doOnNext(i -> System.out.println(i + " ==>" + studentsMap.get(1)))
+                .subscribe();
+
+        sleep(600000);
+    }
+
+    @Test
+    public void appServer2() {
+        Student student1 =  Student.builder().name("sam-updated").age(10).city("atlanta").marks(List.of(1, 2, 3)).build();
+
+        this.studentsMap.put(1, student1);
+    }
 }
 
 
