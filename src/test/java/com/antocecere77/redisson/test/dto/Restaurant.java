85640c7c18c5e17b18d6a2f65ba24f8790223d2f
antocecere77 <antocecere77@gmail.com>
2021-08-19 16:01:42 +0200

    texas restaurant locator

---
+++ src/test/java/com/antocecere77/redisson/test/dto/Restaurant.java
@@ -0,0 +1,23 @@
+package com.antocecere77.redisson.test.dto;
+
+import lombok.*;
+
+@Data
+@With
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Restaurant {
+
+    private String id;
+
+    private String city;
+
+    private double latitude;
+
+    private double longitude;
+
+    private String name;
+
+    private String zip;
+}
